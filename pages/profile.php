c480aefcc1be897719276ec239a09987db08d28d
Joshua Pack <joshua@joshuapack.com>
2015-04-22 15:22:13 -0400

    updated profile page

--- pages/profile.php
+++ pages/profile.php
@@ -1,8 +1,15 @@
-<div id="profile">
-  <div id="profileImage">
-  <div>
-  <div id="profileName">
-  <div>
-  <div id="profileBio">
-  <div>
-<div>
+<?php $profile = $vendor->getVendor(null, $uri[2]); ?>
+<?php if ($profile): ?>
+	<?php $vendor->setVendorCookie($uri[2]); ?>
+	<div id="profile">
+	  <div id="profileImage">
+	    <img src="<?php echo $profile['photo']; ?>" alt="<?php echo $profile['name']; ?>">
+	  <div>
+	  <div id="profileName">
+	    <?php echo $profile['name']; ?>
+	  <div>
+	  <div id="profileBio">
+	    <?php echo $profile['description']; ?>
+	  <div>
+	<div>
+<?php endif; ?>
