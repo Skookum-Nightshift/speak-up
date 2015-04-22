ccb02facfe37d6d4cf2e577860dfdc837815d4b2
Joshua Pack <joshua@joshuapack.com>
2015-04-22 15:22:30 -0400

    updated vendor page

--- pages/vendor.php
+++ pages/vendor.php
@@ -1,10 +1,19 @@
-<div id="vendor">
-  <div id="vendorName">
-  </div>
-  <div id="vendorInfo">
-  </div>
-  <div id="vendorLink">
-  </div>
-  <div id="vendorImage">
-  </div>
-<div>
+<?php $allVendors = $vendor->getAllVendors(3); ?>
+<?php if ($allVendors): ?>
+	<?php while ($vendors = $allVendors->fetch_assoc()): ?>
+	<div id="vendor">
+	  <div id="vendorName">
+	  	<?php echo $vendors['name']; ?>
+	  </div>
+	  <div id="vendorInfo">
+	  	<?php //echo $vendors['description']; ?>
+	  </div>
+	  <div id="vendorLink">
+	  	<a href="/profile/<?php echo $vendors['url']; ?>">Click on <?php echo $vendors['name']; ?> profile</a>
+	  </div>
+	  <div id="vendorImage">
+	  	<img src="<?php echo $vendors['photo']; ?>" alt="<?php echo $vendors['name']; ?>">
+	  </div>
+	</div>
+	<?php endwhile; ?>
+<?php endif; ?>
