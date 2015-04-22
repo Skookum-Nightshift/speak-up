77a3496a437f825809f1c186fa9b42af836c17bd
Joshua Pack <joshua@joshuapack.com>
2015-04-22 16:16:55 -0400

    add about page and updated nav with links

--- template/nav.php
+++ template/nav.php
@@ -1,8 +1,11 @@
 <div id="nav">
   <div id="navBuy">
+    <a href="/buy">Buy</a>
   </div>
   <div id="navLocate">
+    <a href="/vendor">Vendor</a>
   </div>
   <div id="navAbout">
+    <a href="/about">About</a>
   </div>
 <div>
