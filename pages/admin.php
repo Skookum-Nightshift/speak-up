7d148c3a0abf1ae106007c2b56747d56b1eb21d4
Joshua Pack <joshua@joshuapack.com>
2015-04-23 11:33:50 -0400

    updated some login info

--- pages/admin.php
+++ pages/admin.php
@@ -1,3 +1,11 @@
+<?php if ($uri[2] == 'login'): ?>
+	<?php if ($_POST['login']): ?>
+	<?php endif; ?>
+	<?php if ($_POST['create']): ?>
+		<?php echo '<pre>' . print_r($_POST, true) . '</pre>'; ?>
+	<?php endif; ?>
+<?php endif; ?>
+
 <div id="adminform">
 	<form action="/admin/login" method="POST" id="login">
 		<div id="email">
@@ -8,7 +16,7 @@
 			Password:<br />
 			<input type="password" name="password">
 		</div>
-		<input type="submit" value="Log in"> <input type="submit" value="Create Account">
+		<input type="submit" name="login" value="Log in"> <input type="submit" name="create" value="Create Account">
 	</form>
 	<div id='forgotpassword'>
 		<a href="/forgot">Forgot Password?</a>
