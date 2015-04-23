48173157ba0d1323ae2c7d013dd973917adbb3c3
Joshua Pack <joshua@joshuapack.com>
2015-04-23 09:07:34 -0400

    added admin page

---
+++ pages/admin.php
@@ -0,0 +1,9 @@
+<center>
+	<form action="/admin/login" method="POST">
+		Email:<br />
+		<input type="text" name="email"><br />
+		Password:<br />
+		<input type="password" name="password"><br />
+		<input type="submit" value="Submit">
+	</form>
+</center>
