f317f17f6acc0f1a88281a749e9d9c8a1b1f8675
Joshua Pack <joshua@joshuapack.com>
2015-05-07 15:37:34 -0400

    updated admin nav and admin pages with new icons

--- pages/admin/zines.php
+++ pages/admin/zines.php
@@ -2,4 +2,37 @@
   <center>
   	<h1>Zines</h1>
   </center>
+  <div class="panel panel-default">
+    <div class="panel-heading">Zines</div>
+    <table class="table">
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Price</th>
+        <th>Download Link</th>
+        <th>Publisher</th>
+      </tr>
+      <tr>
+        <td><img src="/images/zine/Z6-CLT.jpg" class="img-responsive img-thumbnail center-block" style="width:100px;"></td>
+        <td>December 2014</td>
+        <td>$1.00</td>
+        <td><a href="/zine/Z6-CLT.pdf">Download <span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span></a></td>
+        <td>Speak Up</td>
+      </tr>
+      <tr>
+        <td><img src="/images/zine/Z8-CLT.jpg" class="img-responsive img-thumbnail center-block" style="width:100px;"></td>
+        <td>Vol 2 | Issue 2</td>
+        <td>$2.00</td>
+        <td><a href="/zine/Z8-CLT.pdf">Download <span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span></a></td>
+        <td>Speak Up</td>
+      </tr>
+      <tr>
+        <td><img src="/images/zine/Z9-CLT.jpg" class="img-responsive img-thumbnail center-block" style="width:100px;"></td>
+        <td>Speak Up Issue 6</td>
+        <td>$1.00</td>
+        <td><a href="/zine/Z9-CLT.pdf">Download <span class="glyphicon glyphicon-download-alt" aria-hidden="true"></span></a></td>
+        <td>Speak Up</td>
+      </tr>
+    </table>
+  </div>
 </div>
--- template/admin/nav.php
+++ template/admin/nav.php
@@ -13,24 +13,24 @@
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li<?php if (!isset($uri[2]) || $uri[2] == '' || $uri[2] == 'dashboard') echo ' class="active"'; ?>><a href="/admin/dashboard">Dashboard</a></li>
-        <li<?php if (isset($uri[2]) && $uri[2] == 'vendors') echo ' class="active"'; ?>><a href="/admin/vendors">Vendors</a></li>
-        <li<?php if (isset($uri[2]) && $uri[2] == 'zines') echo ' class="active"'; ?>><a href="/admin/zines">Zines</a></li>
-        <li<?php if (isset($uri[2]) && $uri[2] == 'reports' && $uri[3] == 'transactions') echo ' class="active"'; ?>><a href="/admin/reports/transactions">Transactions</a></li>
-        <li><a href="/admin/settings">Settings</a></li>
+        <li<?php if (!isset($uri[2]) || $uri[2] == '' || $uri[2] == 'dashboard') echo ' class="active"'; ?>><a href="/admin/dashboard">Dashboard <span class="glyphicon glyphicon-dashboard"></span></a></li>
+        <li<?php if (isset($uri[2]) && $uri[2] == 'vendors') echo ' class="active"'; ?>><a href="/admin/vendors">Vendors <span class="glyphicon glyphicon-tasks"></span></a></li>
+        <li<?php if (isset($uri[2]) && $uri[2] == 'zines') echo ' class="active"'; ?>><a href="/admin/zines">Zines <span class="glyphicon glyphicon-briefcase"></span></a></li>
+        <li<?php if (isset($uri[2]) && $uri[2] == 'reports' && $uri[3] == 'transactions') echo ' class="active"'; ?>><a href="/admin/reports/transactions">Transactions <span class="glyphicon glyphicon-transfer"></span></a></li>
+        <li<?php if (isset($uri[2]) && $uri[2] == 'reports' && $uri[3] == 'settings') echo ' class="active"'; ?>><a href="/admin/settings">Settings <span class="glyphicon glyphicon-cog"></span></a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Reports <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
-            <li<?php if (isset($uri[2]) && $uri[2] == 'reports' && $uri[3] == 'vendor') echo ' class="active"'; ?>><a href="/admin/reports/vendor">Vendor Report</a></li>
-            <li<?php if (isset($uri[2]) && $uri[2] == 'reports' && $uri[3] == 'transactions') echo ' class="active"'; ?>><a href="/admin/reports/transactions">Transactions Report</a></li>
-            <li<?php if (isset($uri[2]) && $uri[2] == 'reports' && $uri[3] == 'analytics') echo ' class="active"'; ?>><a href="/admin/reports/analytics">Site Analytics</a></li>
+            <li<?php if (isset($uri[2]) && $uri[2] == 'reports' && $uri[3] == 'vendor') echo ' class="active"'; ?>><a href="/admin/reports/vendor"><span class="glyphicon glyphicon-list-alt"></span> Vendor Report</a></li>
+            <li<?php if (isset($uri[2]) && $uri[2] == 'reports' && $uri[3] == 'transactions') echo ' class="active"'; ?>><a href="/admin/reports/transactions"><span class="glyphicon glyphicon-transfer"></span> Transactions Report</a></li>
+            <li<?php if (isset($uri[2]) && $uri[2] == 'reports' && $uri[3] == 'analytics') echo ' class="active"'; ?>><a href="/admin/reports/analytics"><span class="glyphicon glyphicon-stats"></span> Site Analytics</a></li>
             <li class="divider"></li>
-            <li<?php if (isset($uri[2]) && $uri[2] == 'reports' && $uri[3] == 'settings') echo ' class="active"'; ?>><a href="/admin/reports/settings">Report Settings</a></li>
+            <li<?php if (isset($uri[2]) && $uri[2] == 'reports' && $uri[3] == 'settings') echo ' class="active"'; ?>><a href="/admin/reports/settings"><span class="glyphicon glyphicon-wrench"></span> Report Settings</a></li>
           </ul>
         </li>
-        <li<?php if (isset($uri[2]) && $uri[2] == 'logout') echo ' class="active"'; ?>><a href="/admin/logout">Logout</a></li>
+        <li<?php if (isset($uri[2]) && $uri[2] == 'logout') echo ' class="active"'; ?>><a href="/admin/logout">Logout <span class="glyphicon glyphicon-off"></span></a></li>
       </ul>
     </div>
 
