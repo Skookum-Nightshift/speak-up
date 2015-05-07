f76dbdc31aef17373d308f937b870356218089e2
Joshua Pack <joshua@joshuapack.com>
2015-05-07 15:14:15 -0400

    added forgot password page

---
+++ pages/forgot.php
@@ -0,0 +1,8 @@
+<div class="container-fluid">
+  <div class="page-header center-block row">
+    <div class="col-sm-10 col-sm-offset-1 text-center">
+      <h1>Forgot Password?</h1>
+      <div class="alert alert-danger" role="alert">That is super unfortunate. No way to reset. Contact Admin.</div>
+    </div>
+  </div>
+</div>
